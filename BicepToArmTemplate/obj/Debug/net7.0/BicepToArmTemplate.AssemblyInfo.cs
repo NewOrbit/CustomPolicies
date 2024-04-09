b56b3402d925e4829ea13f351c2a4ac7bc637929
Jay Gregory <jgregory@neworbit.co.uk>
2024-04-09 15:05:15 +0100

    regen bicep

--- BicepToArmTemplate/obj/Debug/net7.0/BicepToArmTemplate.AssemblyInfo.cs
+++ BicepToArmTemplate/obj/Debug/net7.0/BicepToArmTemplate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BicepToArmTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05dd09befabb84996f204225e401d037ecbcbf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5735bb40b4262af0f0f6448028e1369dede4d392")]
 [assembly: System.Reflection.AssemblyProductAttribute("BicepToArmTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BicepToArmTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
